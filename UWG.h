dc4985e844055e9c49be97ec67ce80cb5852219d
sberlinches <sergioberlinches@gmail.com>
2018-07-20 21:57:36 -0700

    is connected method

--- UWG.h
+++ UWG.h
@@ -27,11 +27,14 @@ struct _UWG {
 class UWG {
 private:
     vector<_UWG>* graph;
+    unsigned int numberOfVertices;
+    unsigned int numberOfEdges;
 public:
     UWG(unsigned int);
     vector<_UWG>* getGraph() const;
     vector<unsigned int>* adjacent(unsigned int);
     void insertEdge(unsigned int, unsigned int, unsigned int);
+    bool isConnected();
     void print();
     ~UWG();
 };
@@ -41,6 +44,8 @@ public:
  * @param numberOfVertices The number of vertices
  */
 UWG::UWG(unsigned int numberOfVertices) {
+
+    this->numberOfVertices = numberOfVertices;
 
     // Initializes an empty list of vertices
     graph = new vector<_UWG>(numberOfVertices);
@@ -85,6 +90,50 @@ vector<unsigned int>* UWG::adjacent(unsigned int vertex) {
   */
 void UWG::insertEdge(unsigned int vertex, unsigned int toVertex, unsigned int weight) {
     graph->at(vertex).edges->push_back({toVertex, weight});
+    numberOfEdges++;
+}
+
+/**
+ * Checks and returns whether the graph is connected or not.
+ * @return Whether the graph is connected or not
+ */
+bool UWG::isConnected() {
+
+    // 1. if there's more vertices than edges, definitely is not connected
+    if(numberOfVertices > numberOfEdges)
+        return false;
+
+    // 2. check if all the vertices are connected using the union-find algorithm
+    // TODO: This implementation doesn't seems efficient
+    unsigned int uf[graph->size()];
+
+    for (unsigned int i = 0; i < graph->size(); i++)
+        uf[i] = i;
+
+    for (unsigned int i = 0; i < graph->size(); i++) {
+        for (unsigned int j = 0; j < graph->at(i).edges->size(); j++) {
+
+            unsigned int prevVal = uf[i];
+
+            for (unsigned int k = 0; k < graph->size(); k++)
+                if(uf[k] == prevVal)
+                    uf[k] = uf[graph->at(i).edges->at(j).vertex];
+
+            // DEBUG
+            /*cout << prevVal << "->" << graph->at(i).edges->at(j).vertex << " = ";
+            for (unsigned int l = 1; l < graph->size(); l++)
+                cout << uf[l];
+            cout << endl;*/
+        }
+    }
+
+    // 3. Compares that all the values are the same
+    unsigned int firstVal = uf[0];
+    for (unsigned int i = 1; i < graph->size(); i++)
+        if(uf[i] != firstVal)
+            return false;
+
+    return true;
 }
 
 /**
